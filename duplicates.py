571b3d7d8d504cef19bdca2fe57236a023dabb88
mixxuss <mixxuss@gmail.com>
2017-01-13 17:29:47 +0300

    start

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,11 @@
-
+from os import listdir, stat
 
 def are_files_duplicates(file_path1, file_path_2):
     pass
 
 
 if __name__ == '__main__':
-    pass
+    path = '/Users'
+    filesize = stat('/Users/admin/Desktop/DBR.1.7.50.162.exe')
+    print(filesize.st_size)
+    print(listdir(path))
